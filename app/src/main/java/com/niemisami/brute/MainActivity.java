1c125c0572f2ec6e05392624bc1c661c399eac11
niemisami <sami@nieminen.fi>
2016-09-03 01:25:09 +0300

    added fragment inflation to main activity

--- app/src/main/java/com/niemisami/brute/MainActivity.java
+++ app/src/main/java/com/niemisami/brute/MainActivity.java
@@ -1,7 +1,10 @@
 package com.niemisami.brute;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.niemisami.brute.views.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inflateFragments();
+    }
+
+    /**
+     * Find fragment layout and add it to the activity
+     */
+    private void inflateFragments() {
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager.findFragmentById(R.id.container_body) == null) {
+            MainFragment fragment = new MainFragment();
+            manager.beginTransaction()
+                    .add(R.id.container_body, fragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/niemisami/brute/views/MainFragment.java
+++ app/src/main/java/com/niemisami/brute/views/MainFragment.java
@@ -1,4 +1,4 @@
-package com.niemisami.brute;
+package com.niemisami.brute.views;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.niemisami.brute.R;
 
 
 /**
@@ -15,7 +17,6 @@ public class MainFragment extends Fragment {
 
 
     public MainFragment() {
-        // Required empty public constructor
     }
 
 
